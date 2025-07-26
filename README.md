472f605c3600695967ca976cf83ebeb79449d80b
Yabibi09 <bashasha@naver.com>
2025-07-26 23:59:56 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nurse Off Request App
+
+A simple React + Firebase app for nurse day-off scheduling.
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// Firebase config
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC7mj6LzGEzlm3mxRilUMtvAKc-PmG_cj0",
+  authDomain: "nurse-off.firebaseapp.com",
+  projectId: "nurse-off",
+  storageBucket: "nurse-off.firebasestorage.app",
+  messagingSenderId: "754923113150",
+  appId: "1:754923113150:web:94f069f252260edcdfecef"
+};
+const app = initializeApp(firebaseConfig);
+export default app;
