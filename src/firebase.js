173a36fdd7162546585849c21856af01aec7bce3
Yabibi09 <bashasha@naver.com>
2025-07-27 01:23:18 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
+// 기존 코드
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7mj6LzGEzlm3mxRilUMtvAKc-PmG_cj0",
-  authDomain: "nurse-off.firebaseapp.com",
-  projectId: "nurse-off",
-  storageBucket: "nurse-off.firebasestorage.app",
-  messagingSenderId: "754923113150",
-  appId: "1:754923113150:web:94f069f252260edcdfecef"
+  apiKey: "xxx",
+  authDomain: "xxx.firebaseapp.com",
+  projectId: "xxx",
+  storageBucket: "xxx.appspot.com",
+  messagingSenderId: "xxx",
+  appId: "xxx"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };
+// ✅ 여기를 꼭 추가!
+export default app;
